7d0b8d900c4d7c9191d7a5b723daa938c5fba645
Kimdohyeong12 <hyeong030909@gmail.com>
2025-07-17 19:45:58 +0900

    (2025.7.17)연산자

--- Class1th(Complie)/source.c
+++ Class1th(Complie)/source.c
@@ -68,10 +68,29 @@ void main()
 	
 	// 변수는 프로그램이 실행되는 동안 값을 바꿀 수 있으며,
 	// 원래 저장되어 있는 값은 새로 저장되는 값에 의해 지워집니다.
+    
+	// int experience = 100;
 
-	int experience = 100;
+	// experience = 200;
 
-	experience = 200;
+	// printf("experience : %d\n", experience);
 
-	printf("experience : %d\n", experience);
+#pragma region 상수
+	// 프로그램이 실행되는 동안 더 이상 변경 할 수 없는
+	// 메모리 공간입니다.
+
+	// const float pi = 3.141592f;
+
+	// 상수의 경우 메모리 공간을 가지고 있지 않은 상수를
+	//리터럴 상수라고 하며, 메모리 공간을 가지고 있는 
+	//상수를 심볼릭 상수라고 합니다.
+
+	// ex) pi = 6.125f;
+
+	// printf("pi의 값 : %f\n", pi);
+
+    //상수의 메모리 공간을 생성하는 동시에 초기화해야 하며
+   //한 번 저장된 값은 더 이상 변경할 수 없습니다.
+#pragma endregion
+
 }
