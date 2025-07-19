dd50d9e63f063354930aa06c77ac63380e4d5c92
Kimdohyeong12 <hyeong030909@gmail.com>
2025-07-19 20:47:07 +0900

    (2025.7.18)오버플로우와 언더플로우

--- class2th(Variable & constant)/sourse.c
+++ class2th(Variable & constant)/sourse.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
 void main()
-{  
+{
+#pragma region 산술 연산자
+	// 1. 변수 = 변수 + 변수
+	// int x1 = 10;
+	// int x2 = 11;
+	// int ko1 = x1 + x2;
+	// printf("ko1 = %d\n", ko1);
+
+	// 2. 변수 = 변수 - 리터럴 상수
+	// int s1 = 13;
+	// int ko2 = s1 - 7;
+	// printf("ko2 = %d\n", ko2);
+
+	// 3. 변수 = 변수 * 심볼릭 상수
+	// int x3 = 2;
+	// const int x4 = 12;
+	// int ko3 = x3 * x4;
+	// printf("ko3 = %d\n", ko3);
+
+	// 4. 변수 = 리터럴 상수 / 리터럴 상수
+	// int ko4 = 10 / 5;
+	// printf("ko4 = %d\n", ko4);
+
+	// 5. 변수 = 심볼릭 상수 % 리터럴 상수
+	// const int x5 = 8;
+	// int ko5 = x5 % 2;
+	// printf("ko5 = %d\n", ko5);
+
+#pragma endregion
+
 #pragma region 비트
 	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의 조합으로
 	//논리 계산을 수행하는 단위입니다.
@@ -20,6 +49,7 @@ void main()
 	//다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
 #pragma endregion
 
+
 #pragma region 비트 연산자
 	// 비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자입니다.
 
@@ -31,8 +61,8 @@ void main()
 	 //              -----------
 	//                0000 1000
 	
-	int storage1 = 12; // 0000 1100
-	int storage2 = 10; // 0000 1010
+	//int storage1 = 12; // 0000 1100
+	//int storage2 = 10; // 0000 1010
 	     //              -----------               
 		//                0000 1000
 	//printf ("storage1 & storage2 = %d\n", storage1 & storage2);
@@ -53,14 +83,14 @@ void main()
 	// int storage6 = 10;   0000 1010
 	//                     ----------- 
 	//                      0000 0010
-	//printf("storage5  storage6 = %d\n", storage5  storage6);
+	//printf("storage5 ^ storage6 = %d\n", storage5 ^ storage6);
 #pragma endregion
 
 #pragma region  NOT 연산자
 	// 하나의 비트를 반전시키는 연산자입니다.
 	
 	// int storage7 = 11; 0000 1011 --> 1111 0100
-	//printf("~storage7 );
+	//printf("~storage7 = %d\n", ~storage7 );
 
 	//첫 번째 비트는 부호를 나타내며, 첫번째 비트에
 	//1이 있다면 값은 음수입니다.
@@ -72,19 +102,25 @@ void main()
 #pragma endregion
 
 #pragma endregion
-#pragma region 산술 연산자
-	// 1. 변수 = 변수 + 변수
 
-	// 2. 변수 = 변수 - 리터럴 상수
+#pragma region 시프트 연산자
+// 비트를 특정한 위치로 정해진 수만큼 이동시키는 연산자입니다.
 
-	// 3. 변수 = 변수 * 심볼릭 상수
+	// int data = 5; // 0000 0101
 
-	// 4. 변수 = 리터럴 상수 / 리터럴 상수
+	// int shift = data << 2; // 0001 0100
 
-	// 5. 변수 = 심볼릭 상수 % 리터럴 상수
+	// printf("shift 변수의 값 : %d\n", shift);
+
+	// shift = data >> 1; // 0000 0010
+
+	// printf("shift 변수의 값 : %d\n", shift);
 #pragma endregion
 
+
 #pragma endregion
+
+
 
 
 }
