ab10d10c29d431df4dcfde44ce156c2c5fa7bd6f
Kimdohyeong12 <hyeong030909@gmail.com>
2025-08-05 23:35:08 +0900

    (2025.8.5)동적 할당

--- Class11th(storage)/sourse.c
+++ Class11th(storage)/sourse.c
@@ -7,17 +7,28 @@ int main()
 	// 프로그램을 실행하는 중에 필요한 만큼 메모리를 할당
 	// 하는 작업입니다.
 
-	int* pointer = (int *)malloc(4);
-	
-	*pointer = 10;
+	//  int* pointer = (int *)malloc(4);
+	// 
+	// *pointer = 10;
+	// 
+	//  printf("pointer 가리키는 값 : %d\n", *pointer);
+	// 
+	int* pointer = (int*)malloc(sizeof(int) * 5);
 
-	printf("pointer 가리키는 값 : %d\n", *pointer);
+	for (int i = 0; i < 3; i++);
+	{
+		pointer[i] = (i + 1) * 10;
+
+		printf("pointer[%d] : %d\n", i, pointer[i]);
+	}
+
+	free(pointer);
  
 	// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
 	// 변경할 수 있으며, 동적으로 메모리의 크기를 할당할 때
 	// 바이트 단위로 지정합니다.
 
-	free(pointer);
+	// free(pointer);
 
 	// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로,
 	// 사용이 끝나면 직접 해제해주어야 합니다.
