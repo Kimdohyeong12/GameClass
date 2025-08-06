e8d7d757e3b34961cbd3a3419aa0b39338b777c2
Kimdohyeong12 <hyeong030909@gmail.com>
2025-08-06 19:45:32 +0900

    (2025.8.6)구조체

--- Class11th(storage)/sourse.c
+++ Class11th(storage)/sourse.c
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void main()
 {
 #pragma region 동적 할당
 	// 프로그램을 실행하는 중에 필요한 만큼 메모리를 할당
@@ -13,16 +13,29 @@ int main()
 	// 
 	//  printf("pointer 가리키는 값 : %d\n", *pointer);
 	// 
-	int* pointer = (int*)malloc(sizeof(int) * 5);
+	//  int* pointer = (int*)malloc(sizeof(int) * 5);
+	//  
+	//  for (int i = 0; i < 3; i++);
+	//  {
+	//  	pointer[i] = (i + 1) * 10;
+	//  
+	//  	printf("pointer[%d] : %d\n", i, pointer[i]);
+	//  }
+	// 
+	// free(pointer);
+	// 
+	// pointer = (int*)malloc(sizeof(int) * 5);
+	// 
+	// pointer[0] = 10;
+	// pointer[1] = 20;
+	// pointer[2] = 30;
+	// 
+	// for (int i = 0; i < 3; i++);
+	// {
+	// 	printf("pointer[%d] : %d\n", i, pointer[i]);
+	// }
 
-	for (int i = 0; i < 3; i++);
-	{
-		pointer[i] = (i + 1) * 10;
-
-		printf("pointer[%d] : %d\n", i, pointer[i]);
-	}
-
-	free(pointer);
+	// free(pointer);
  
 	// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
 	// 변경할 수 있으며, 동적으로 메모리의 크기를 할당할 때
@@ -34,6 +47,26 @@ int main()
 	// 사용이 끝나면 직접 해제해주어야 합니다.
 
 #pragma endregion
+
+#pragma region 허상 포인터
+	// 이미 해제된 메모리 영역을 가리키는 포인터입니다.
+
+	// int* address =(int *)malloc(sizeof(int));
+	// 
+	// *address = 300;
+	// 
+	// printf("addres 변수가 가리키는 값 : %d\n", *address);
+	// 
+	// free(address);
+	// 
+	// address = NULL;
+	// 
+	// *address = 100;
+	// 
+	// printf("addres 변수가 가리키는 값 : %d\n", *address);
+
+#pragma endregion
+
 
 
 	return 0;
---
+++ Class12th (Dynamic Allocation)/sourse.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+struct Data
+{
+	char grade;
+	int health;
+	double experience;
+};
+
+int main()
+{
+#pragma region 구조체
+	// 여러 개의 변수를 하나의 집합으로 구조화한 다음
+	// 하나의 객체를 생성하는 것입니다.
+
+	// struct Data data = {'B', 50, 75.625 };
+	// 
+	// printf("data의 grade : %c\n", data.grade);
+	// printf("data의 health : %d\n", data.health);
+	// printf("data의 experience : %lf\n\n", data.experience);
+	// // 구조체의 각 멤버는 구조체 선언에서 나타나는 순서대로 초기화됩니다.
+	// // 이 순서는 초기화 목록의 값들이 왼쪽서부터 오른쪽으로 대응됩니다.
+	// 
+	// data.grade = 'A';
+	// data.health = 100;
+	// data.experience = 135.75;
+	// 
+	// printf("data의 grade : %c\n", data.grade);
+	// printf("data의 health : %d\n", data.health);
+	// printf("data의 experience : %lf\n", data.experience);
+
+	// 구조체를 선언하기 전에 구조체는 메모리 공간이
+	// 생성되지 않으므로, 구조체 내부에 있는 데이터를
+	// 초기화 할 수 있습니다.
+#pragma endregion
+
+#pragma region 두 점 사이의 거리
+	
+	// printf( "%lf\n", sqrt(49));
+	// 
+	// printf("%lf\n", pow(2,2));
+
+
+
+
+
+#pragma endregion
+
+
+	return 0;
+}
+
